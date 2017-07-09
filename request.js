2f18ca608abacb85ef41828064ad5a69b828279a
Praveen Peruri <praveenperuri@Praveens-MBP.fios-router.home>
2017-07-08 22:15:38 -0500

    Final Commit

--- request.js
+++ request.js
@@ -16,28 +16,28 @@ inquirer.prompt([
     {
         type: "list",
         name: "requestWhat",
-        message: "What is your request??",
-        choices: ["Search for a movie?", "Search in Twitter?", "Search in Spotify?", "Get geocode of a location?", "Get weather at a location?"]
+        message: "HI !! What's your request??",
+        choices: ["Search a movie?", "Search in Twitter?", "Search in Spotify?", "Get geocode of a location?", "Get weather of a location?"]
     }
 ]).then(function(user) {
     switch (user.requestWhat) {
         case "Search in Spotify?":
             inputSpotifyRequest();
             break;
-        case "Search for a movie?":
+        case "Search a movie?":
             inputMovieRequest();
             break;
         case "Search in Twitter?":
             inputTwitterRequest();
             break;
         case "Get geocode of a location?":
-        	getGeocodeRequest();
-        	break;
-        case "Get weather at a location?":
-        	getWeatherRequest();
-        	break;
+            getGeocodeRequest();
+            break;
+        case "Get weather of a location?":
+            getWeatherRequest();
+            break;
         default:
-            console.log("BAD REQUEST");
+            console.log("Something's wrong !! Try Again !!");
             break;
     }
 });
@@ -89,7 +89,7 @@ function getSpotifySearchTerm(type) {
             } else {
                 searchString = args[0];
             }
-            console.log(searchString);
+            //console.log(searchString);
             getSpotifyResults(type, searchString);
         }
     });
@@ -119,7 +119,7 @@ function getSpotifyResults(type, input) {
         json: true
     };
 
-    console.log(authOptions);
+    //console.log(authOptions);
 
     request.post(authOptions, function(error, response, body) {
         if (!error && response.statusCode === 200) {
@@ -136,7 +136,9 @@ function getSpotifyResults(type, input) {
             request.get(options, function(error, response, body) {
                 if (!error && response.statusCode === 200) {
                     console.log(body);
-                    //console.log(body.artists.items[0]);
+
+
+
                 }
             });
         }
@@ -159,8 +161,7 @@ function inputMovieRequest() {
 }
 
 function getMovieResults(input) {
-    //make api call and log results	
-    //omdbrequest
+
     var args = input.split(' ');
     var movieName = "";
 
@@ -179,12 +180,10 @@ function getMovieResults(input) {
     // Then run a request to the OMDB API with the movie specified
     request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
-        // If the request is successful (i.e. if the response status code is 200)
         if (!error && response.statusCode === 200) {
 
             console.log(JSON.parse(body));
-            // Parse the body of the site and recover just the imdbRating
-            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+
             console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
         }
     });
@@ -224,7 +223,6 @@ function getTwitterResults(input) {
         if (!error && response.statusCode === 200) {
             var result = tweets;
             // Then log the body from the site!
-            //console.log(tweets);
             for (i = 0; i < tweets.statuses.length; i++) {
                 console.log('Tweet: ' + tweets.statuses[i].text);
                 console.log('Favorite Count: ' + tweets.statuses[i].favorite_count);
@@ -239,26 +237,23 @@ function getTwitterResults(input) {
 
 function getGeocodeRequest() {
 
-	inquirer.prompt([
+    inquirer.prompt([
 
-  {
-    type: "input",
-    name: "userInput",
-    message: "Which location or landmark would you like to geocode?"
-  }
+        {
+            type: "input",
+            name: "userInput",
+            message: "Which location or landmark would you like to geocode?"
+        }
 
-// After the prompt, store the user's response in a variable called location.
-]).then(function(location) {
+    ]).then(function(location) {
 
-  // console.log(location.userInput);
 
-  // Then use the Google Geocoder to Geocode the address
-  geocoder.geocode(location.userInput, function(err, data) {
+        geocoder.geocode(location.userInput, function(err, data) {
 
-    console.log(JSON.stringify(data, null, 2));
-  });
+            console.log(JSON.stringify(data, null, 2));
+        });
 
-});
+    });
 
 }
 
@@ -266,29 +261,27 @@ function getGeocodeRequest() {
 
 function getWeatherRequest() {
 
-  inquirer.prompt([
-  {
-    type: "input",
-    name: "userInput",
-    message: "Type location or landmark to get current weather?"
-  }
-]).then(function(location) {
+    inquirer.prompt([{
+        type: "input",
+        name: "userInput",
+        message: "Type location or landmark to get current weather?"
+    }]).then(function(location) {
 
-	if(location.userInput !== undefined && location.userInput !== null) {
+        if (location.userInput !== undefined && location.userInput !== null) {
 
-		weather.find({ search: location.userInput, degreeType: "F" }, function(err, result) {
+            weather.find({ search: location.userInput, degreeType: "F" }, function(err, result) {
 
-		  if (err) {
-		    console.log(err);
-		  }
-		  console.log(JSON.stringify(result, null, 4));
+                if (err) {
+                    console.log(err);
+                }
+                console.log(JSON.stringify(result, null, 4));
 
-		});
-	} else {
-		console.log('Please make a valid entry !!')
-	}
+            });
+        } else {
+            console.log('Please make a valid entry !!')
+        }
 
-});
+    });
 
 
 }
